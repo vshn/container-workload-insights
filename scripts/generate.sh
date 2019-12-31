5a53c9b92b15b5d1849c73000d52db98925eaedf
Joao Pinto <joao.pinto@vshn.ch>
2019-12-31 15:34:45 +0100

    Move core stats to it's own file

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+openpipe run pipelines/stats/openshift_managed_processes.yaml ../appuio-workload-insights/pic/output/
 openpipe run pipelines/stats/databases.yaml
 openpipe run pipelines/stats/languages.yaml
 openpipe run pipelines/stats/webservers.yaml

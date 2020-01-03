0ef598f513380790b335d926744c52a00f142211
Joao Pinto <joao.pinto@vshn.ch>
2020-01-03 10:25:53 +0100

    Fix generate script path

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-openpipe run pipelines/stats/openshift_managed_processes.yaml ../appuio-workload-insights/pic/output/
+mkdir -p output
+openpipe run pipelines/stats/openshift_managed_processes.yaml ../pic/output/
 openpipe run pipelines/stats/databases.yaml
 openpipe run pipelines/stats/languages.yaml
 openpipe run pipelines/stats/webservers.yaml

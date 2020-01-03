991b6ec73bc66e295def6c64f06b23318762b2ba
Joao Pinto <joao.pinto@vshn.ch>
2020-01-03 10:32:44 +0100

    Added images dir mkdir

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 mkdir -p output
+mkdir -p docs/images
 openpipe run pipelines/stats/openshift_managed_processes.yaml ../pic/output/
 openpipe run pipelines/stats/databases.yaml
 openpipe run pipelines/stats/languages.yaml
